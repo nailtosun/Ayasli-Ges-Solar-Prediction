389ce736b9efa62d9224fbe3763e3380be4473af
DESKTOP-6IJJOUF\nailt <nailtosun07@gmail.com>
2018-07-17 16:50:04 +0300

    arima and function fitting added

--- Neural Network/Scripts/onehot_encoded.m
+++ Neural Network/Scripts/onehot_encoded.m
@@ -1,23 +1,23 @@
-for i = 1:4982    
-    if PAC(i) >= 1 && PAC(i) < 5000
+for i = 1:30354
+    if data(i,17) > 0 && data(i,17) < 500
         newEncodedVector(i,:) = [0 0 0 0 0 0 0 0 0 0];
-    elseif PAC(i) >= 5000 && PAC(i) < 10000
+    elseif data(i,17) >= 500 && data(i,17) < 1000
         newEncodedVector(i,:) = [0 0 0 0 0 0 0 0 1 0];
-    elseif PAC(i) >= 10000 && PAC(i) < 15000
+    elseif data(i,17) >= 1000 && data(i,17) < 2500
         newEncodedVector(i,:) = [0 0 0 0 0 0 0 1 0 0];
-    elseif PAC(i) >= 15000 && PAC(i) < 20000
+    elseif data(i,17) >= 2500 && data(i,17) < 5000
         newEncodedVector(i,:) = [0 0 0 0 0 0 1 0 0 0];
-    elseif PAC(i) >= 20000 && PAC(i) < 25000
+    elseif data(i,17) >= 5000 && data(i,17) < 8000
         newEncodedVector(i,:) = [0 0 0 0 0 1 0 0 0 0];
-    elseif PAC(i) >= 25000 && PAC(i) < 30000
+    elseif data(i,17) >= 8000 && data(i,17) < 10000
         newEncodedVector(i,:) = [0 0 0 0 1 0 0 0 0 0];
-    elseif PAC(i) >= 30000 && PAC(i) < 35000
+    elseif data(i,17) >= 10000 && data(i,17) < 12500
         newEncodedVector(i,:) = [0 0 0 1 0 0 0 0 0 0];
-    elseif PAC(i) >= 35000 && PAC(i) < 40000
+    elseif data(i,17) >= 12500 && data(i,17) < 15000
         newEncodedVector(i,:) = [0 0 1 0 0 0 0 0 0 0];
-    elseif PAC(i) >= 40000 && PAC(i) < 45000
+    elseif data(i,17) == 0
         newEncodedVector(i,:) = [0 1 0 0 0 0 0 0 0 0];
-    else
+    elseif data(i,17) >= 15000 && data(i,17) < 40000
         newEncodedVector(i,:) = [1 0 0 0 0 0 0 0 0 0];
     end
 end
